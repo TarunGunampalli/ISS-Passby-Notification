7eb122d2d854f34cbf27bcc83b9e43519d6a7070
TarunGunampalli <34664042+TarunGunampalli@users.noreply.github.com>
2020-08-28 11:05:20 -0500

    Added function for cleanliness

--- ISS.py
+++ ISS.py
@@ -3,9 +3,7 @@ import json
 from datetime import datetime
 from datetime import time
 from twilio.rest import Client
-
-def checkTime(target_date):
-    if (date.today != target_date)
+import time
 
 #set variables for location of passing
 latitude = 27.2038
@@ -13,20 +11,56 @@ longitude = -77.5011
 altitude = 189
 passes = 1
 
-response = requests.get("http://api.open-notify.org/iss-pass.json", params = {'lat':latitude, 'lon':longitude, 'alt':altitude, 'n':passes})
-response = json.loads(response.text)
-risetime = response['response'][0]['risetime']
+def checkToday(target_date, time):
+    time_diff = datetime.timestamp(target_date) - datetime.timestamp(date.today())
+    if time_diff < time and time_diff > 0:
+        return True;
+    else:
+        return False
 
+def get_risetime():
+    response = requests.get("http://api.open-notify.org/iss-pass.json", params = {'lat':latitude, 'lon':longitude, 'alt':altitude, 'n':passes})
+    response = json.loads(response.text)
+    risetime = response['response'][0]['risetime']
+    return risetime
+
+def get_duration():
+    response = requests.get("http://api.open-notify.org/iss-pass.json", params = {'lat':latitude, 'lon':longitude, 'alt':altitude, 'n':passes})
+    response = json.loads(response.text)
+    duration = response['response'][0]['duration']
+    return duration
+
+def send_message(message):
+    message = client.messages.create(
+                    body=message,
+                    from_=from_number,
+                    to=to_number
+                )
+    
+def seconds_to_minutes(seconds):
+    minutes = seconds // 60
+    seconds %= 60
+    return str(minutes) + " minutes and " + str(seconds) + " seconds"
+
+risetime = get_risetime()
+risetime = 1598597995
+duration = get_duration()
+duration_minutes = seconds_to_minutes(duration)
 date = datetime.fromtimestamp(risetime)
 
 account_sid = 'AC01719c32016ba6324d2684f4287edb3e'
-auth_token = '580d6ec5993b9f596dcdfbc88a22409f'
+auth_token = '0'
 from_number = '+12029534316'
 to_number = '+14692880626'
 client = Client(account_sid, auth_token)
 
-message = client.messages.create(
-                     body="The ISS will pass next on " + date.strftime("%A, %B %d, %Y ") + "at " + date.strftime("%I:%M"),
-                     from_=from_number,
-                     to=to_number
-                 )
+while True:
+    if not checkToday(date, 3600):
+        time.sleep(risetime - 3600 - datetime.timestamp(date.today()))
+        send_message("The ISS will pass next in 1 hour")
+    elif not checkToday(date, 300):
+        time.sleep(risetime - 300 - datetime.timestamp(date.today()))
+        send_message("The ISS will pass next in 5 minutes")
+    else:
+        time.sleep(risetime - datetime.timestamp(date.today()))
+        send_message("The ISS is currently passing over your location. It will be passing for " + duration_minutes)
